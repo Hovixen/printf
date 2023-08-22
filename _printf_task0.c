d275fe1f9975df36b7dc1476b098824e4f011943
Hovixen <orevaerijo@gmail.com>
2023-08-22 12:13:50 +0000

    task 0

--- _printf_task0.c
+++ _printf_task0.c
@@ -39,10 +39,7 @@ int _printf(const char *format, ...)
 				str = va_arg(in_args, char *);
 				write(1, str, _strlen(str));
 				char_count += _strlen(str);
-			} /*else
-			{
-				write(1, f}*/
-		} format++;
+			} format++;
 	} va_end(in_args);
 	return (char_count);
 }
