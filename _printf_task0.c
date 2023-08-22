7bd91ae14fc5238a69eb69cabbc689147df644ae
Hovixen <orevaerijo@gmail.com>
2023-08-22 09:35:07 +0000

    task 0

---
+++ _printf_task0.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _printf - function prints formatted string c, s %.
+ * @format: format argument to be given.
+ * Return: returns the lenght of string without null.
+ */
+
+int _printf(const char *format, ...)
+{
+	int char_count = 0;/*used to count the number of characters*/
+	char c;
+	char *str;
+	va_list in_args;/*initialize the incoming string arguments*/
+
+	va_start(in_args, format);
+
+	if (format == NULL)
+		return (-1);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			char_count++;
+		} else /*if *format == '%'*/
+		{
+			format++;/*go to the next character*/
+			while (*format)
+			{
+				if (*format == 'c')
+				{
+					c = va_arg(in_args, int);
+					write(1, &c, 1);
+					char_count++;
+				} else if (*format == '%')
+				{
+					write(1, format, 1);
+					char_count++;
+				} else if (*format == 's')
+				{
+					str = va_arg(in_args, char *);
+					write(1, str, _strlen(str));
+					char_count += _strlen(str);
+				} format++;
+			}
+		} format++;
+	}
+	va_end(in_args);
+	return (char_count);
+}
